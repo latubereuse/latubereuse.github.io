4bdd92e123b9d57361546145928c17ba1b748374
Ehsan Kia <contact@ehsankia.com>
2018-03-17 18:54:14 -0700

    Convert to spaces

--- new/app.js
+++ new/app.js
@@ -2,22 +2,22 @@ angular
 .module('MaryamApp', [])
 .controller('MaryamCtrl', ['$scope', '$http', '$location',
 function($scope, $http, $location) {
-	$scope.homepage = true;
-	$scope.data = {};
+  $scope.homepage = true;
+  $scope.data = {};
 
-	$scope.setPage = (category) => {
-		$location.path('/' + category.replace(/\s/g, '-'));
-		$scope.category = category;
-		$scope.homepage = false;
-	}
+  $scope.setPage = (category) => {
+    $location.path('/' + category.replace(/\s/g, '-'));
+    $scope.category = category;
+    $scope.homepage = false;
+  }
 
-	$http.get('food.json').then((response) => {
-		$scope.data = response.data;
-	});
+  $http.get('food.json').then((response) => {
+    $scope.data = response.data;
+  });
 
-	$scope.$watch(() => $location.path(), (page) => {
-		page = page.replace('/', '');
-		$scope.homepage = (page === '');
-		$scope.category = page.replace(/-/g, ' ');
-	});
+  $scope.$watch(() => $location.path(), (page) => {
+    page = page.replace('/', '');
+    $scope.homepage = (page === '');
+    $scope.category = page.replace(/-/g, ' ');
+  });
 }]);
